70de94d79837a820a2e57ff00952a87df05e43d9
sumair jarani <sumair_2009@hotmail.com>
2019-09-10 18:37:26 +0500

    Updates

---
+++ precache-manifest.b2d94d17d61f666bf830ce1ab314b3cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec3757f996b01c1d24aaa3067fd3e9a",
+    "url": "/myportfolio-reactjs/index.html"
+  },
+  {
+    "revision": "2e0b18a1653fe1e771a6",
+    "url": "/myportfolio-reactjs/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "65ee229c3a60ed3d6229",
+    "url": "/myportfolio-reactjs/static/css/main.85bdc16a.chunk.css"
+  },
+  {
+    "revision": "2e0b18a1653fe1e771a6",
+    "url": "/myportfolio-reactjs/static/js/2.6cf24a68.chunk.js"
+  },
+  {
+    "revision": "65ee229c3a60ed3d6229",
+    "url": "/myportfolio-reactjs/static/js/main.5523b4e1.chunk.js"
+  },
+  {
+    "revision": "a0b0f4c5546082a83c5d",
+    "url": "/myportfolio-reactjs/static/js/runtime~main.e079b784.js"
+  },
+  {
+    "revision": "9ffa99c4a1d36285ff188773f2e950af",
+    "url": "/myportfolio-reactjs/static/media/avatar.9ffa99c4.png"
+  },
+  {
+    "revision": "7da2141c24576e0ee50f03c78961d988",
+    "url": "/myportfolio-reactjs/static/media/carousel1.7da2141c.jpg"
+  },
+  {
+    "revision": "017a5d9501d852ab36f6689bd007c2f2",
+    "url": "/myportfolio-reactjs/static/media/carousel2.017a5d95.jpg"
+  },
+  {
+    "revision": "d94f8b3110ae9c6abad80f20a057eb0e",
+    "url": "/myportfolio-reactjs/static/media/carousel3.d94f8b31.jpg"
+  },
+  {
+    "revision": "a9934bbe1f67868f002372c452958b03",
+    "url": "/myportfolio-reactjs/static/media/college.a9934bbe.jpg"
+  },
+  {
+    "revision": "cd762e5eecb5a4cb72c5a0024d6cf7a9",
+    "url": "/myportfolio-reactjs/static/media/school.cd762e5e.jpg"
+  },
+  {
+    "revision": "dc35ab30208ced3455d811392fbc2ca2",
+    "url": "/myportfolio-reactjs/static/media/university.dc35ab30.jpg"
+  }
+]);
